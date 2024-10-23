e2eff557affd627eabadffa769ada5badd22334d
keechongwei <keechongwei@gmail.com>
2024-10-23 11:53:40 +0800

    Made Menus for Roles

--- Login.java
+++ Login.java
@@ -91,16 +91,16 @@ public class Login {
             } 
         }
         for(int i = 0; i<staffs.size();i++){
-            String temp = staffs.get(i).get(0);
+            String temp = staffs.get(i).get(2);
             if (staffs.get(i).get(0).equals(ID)){
                 validID = true;
-                if (temp == "Doctor"){
+                if (temp.equals("Doctor")){
                     role = role.Doctor;
                 }
-                else if (temp == "Pharmacist"){
+                else if (temp.equals("Pharmacist")){
                     role = role.Pharmacist;
                 }
-                else if (temp == "Administrator"){
+                else if (temp.equals("Administrator")){
                     role = role.Administrator;
                 }
             } 
@@ -110,7 +110,7 @@ public class Login {
         Scanner sc = new Scanner(System.in);
         String ID;
         String password;
-        initialise();
+        initialise(); // loads in data from csv
         while(!loggedIn){
             System.out.println("HOSPITAL MANAGEMENT SYSTEM LOGIN PAGE");
             System.out.println("Enter Hospital ID: ");
@@ -145,16 +145,139 @@ public class Login {
             loggedIn = true;
         }
         System.out.println("Successful Login!");
+        int choice = 0;
         switch(role){
             case Patient:
-            //implement Patient Menu
+            choice = 0;
+            while(choice != 9){
+                System.out.println("PATIENT MENU, ENTER CHOICE");
+                System.out.println("(1) View Medical Record");
+                System.out.println("(2) Update Personal Information");
+                System.out.println("(3) View Available Appointment Slots");
+                System.out.println("(4) Schedule An Appointment");
+                System.out.println("(5) Reschedule An Appointment");
+                System.out.println("(6) Cancel An Appointment");
+                System.out.println("(7) View Scheduled Appointments");
+                System.out.println("(8) View Past Appointment Outcome Records");
+                System.out.println("(9) Logout");
+                choice = sc.nextInt();
+                switch(choice){
+                    case 1:
+                    // View Medical Record
+                    break;
+                    case 2:
+                    // Update Personal Information
+                    break;
+                    case 3:
+                    // View Available Appointment Slots
+                    break;
+                    case 4:
+                    // Schedule An Appointment
+                    break;
+                    case 5:
+                    // Reschedule An Appointment
+                    break;
+                    case 6:
+                    // Cancel An Appointment
+                    break;
+                    case 7:
+                    // View Scheduled Appointments
+                    break;
+                    case 8:
+                    // View Past Appointments Outcome Record
+                    break;
+                }
+            }
             break;
             case Doctor:
-            //implement Doctor Menu
-            break;
-            case Administrator:
+            choice = 0;
+            while(choice != 8){
+                System.out.println("DOCTOR MENU, ENTER CHOICE");
+                System.out.println("(1) View Patient Medical Record");
+                System.out.println("(2) Update Patient Medical Record");
+                System.out.println("(3) View Personal Schedule");
+                System.out.println("(4) Set Availability For Appointments");
+                System.out.println("(5) Accept Or Decline Appointment Requests");
+                System.out.println("(6) View Upcoming Appointments");
+                System.out.println("(7) Record Appointment Outcome");
+                System.out.println("(8) Logout");
+                choice = sc.nextInt();
+                switch(choice){
+                    case 1:
+                    // View Patient Medical Record
+                    break;
+                    case 2:
+                    // Update Patient Medical Record
+                    break;
+                    case 3:
+                    // View Personal Schedule
+                    break;
+                    case 4:
+                    // Set Availability For Appointments
+                    break;
+                    case 5:
+                    // Accept Or Decline Appointment Requests
+                    break;
+                    case 6:
+                    // View Upcoming Appointments
+                    break;
+                    case 7:
+                    // Record Appointment Outcome
+                    break;
+                }
+            }
             break;
             case Pharmacist:
+            choice = 0;
+            while(choice != 5){
+                System.out.println("PHARMACIST MENU, ENTER CHOICE");
+                System.out.println("(1) View Appointment Outcome Record");
+                System.out.println("(2) Update Prescription Status");
+                System.out.println("(3) View Medication Inventory");
+                System.out.println("(4) Submit Replenishment Request");
+                System.out.println("(5) Logout");
+                choice = sc.nextInt();
+                switch(choice){
+                    case 1:
+                    // View Appointment Outcome  Record
+                    break;
+                    case 2:
+                    // Update Prescription Status
+                    break;
+                    case 3:
+                    // View Medication Inventory
+                    break;
+                    case 4:
+                    // Submit Replenishment Request
+                    break;
+                }
+            }
+            break;
+            case Administrator:
+            choice = 0;
+            while(choice != 5){
+                System.out.println("ADMINISTRATOR MENU, ENTER CHOICE");
+                System.out.println("(1) View and Manage Hospital Staff");
+                System.out.println("(2) View Appointments Details");
+                System.out.println("(3) View and Manage Medication History");
+                System.out.println("(4) Approve Replenishment Requests");
+                System.out.println("(5) Logout");
+                choice = sc.nextInt();
+                switch(choice){
+                    case 1:
+                    // View and Manage Hospital Staff
+                    break;
+                    case 2:
+                    // View Appointments Details
+                    break;
+                    case 3:
+                    // View and Manage Medication History
+                    break;
+                    case 4:
+                    // Approve Replenishment Request
+                    break;
+                }
+            }
             break;
         }
 
