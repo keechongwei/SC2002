2d6202ba8748c3c5ff55eac283bb4f0497667bc6
keechongwei <keechongwei@gmail.com>
2024-11-15 10:05:28 +0800

    modified user class

--- User.java
+++ User.java
@@ -1,7 +1,23 @@
-public class User {
 
-	private string HospitalID;
-	private string Password;
+public class User {
+    enum Role{Unknown,Patient,Doctor,Pharmacist,Administrator}
+
+	private String HospitalID;
+	private String Password;
 	private Role User_Role;
+
+	public User(String HospitalID, String password, Role role){
+		this.HospitalID = HospitalID;
+		this.Password = password;
+		this.User_Role = role;
+	}
+
+	public String getHospitalID(){
+		return this.HospitalID;
+	}
+
+	public String getPassword(){
+		return this.Password;
+	}
 
 }
