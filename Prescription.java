d3e39ea42f9585c9d7818293d09eaa87254cc260
Shawn Lim <shawnlim202@gmail.com>
2024-11-15 16:39:27 +0800

    Pharmacist

--- Prescription.java
+++ Prescription.java
@@ -2,11 +2,13 @@ public class Prescription {
     private String medicationName;
     private PrescriptionStatus status;
     private int dosage;
+    // private Medication medication;
 
     public Prescription(String medicationName, PrescriptionStatus status, int dosage) {
         this.medicationName = medicationName;
         this.status = status;
         this.dosage = dosage;
+        // this.medication = medication;
     }
 
     public String getMedicationName() {
@@ -24,4 +26,25 @@ public class Prescription {
     public void getDosage(int dosage){
         this.dosage = dosage;
     }
+
+    // public Medication getMedication() {
+    //     return medication;
+    // }
+
+    public static Prescription fromCSV(String csvString) { 
+        String[] fields = csvString.split("\\^"); 
+        String medicationName = fields[0]; 
+        PrescriptionStatus status = PrescriptionStatus.valueOf(fields[1]); 
+        int dosage = Integer.parseInt(fields[2]); 
+        return new Prescription(medicationName, status, dosage); 
+    }
+
+    public String toCSV() {
+        // Use ^ as the delimiter
+        return (medicationName != null ? medicationName : "null") + "^" +
+               (status != null ? status.name() : "null") + "^" +
+               dosage;
+    }
+
+
 }
