d562651d3dbe5f24c4242c46290f19de8cfbc5f0
keechongwei <keechongwei@gmail.com>
2024-11-15 10:18:33 +0800

    modified user

--- User.java
+++ User.java
@@ -1,15 +1,12 @@
 
 public class User {
-    enum Role{Unknown,Patient,Doctor,Pharmacist,Administrator}
 
 	private String HospitalID;
 	private String Password;
-	private Role User_Role;
 
-	public User(String HospitalID, String password, Role role){
+	public User(String HospitalID, String password){
 		this.HospitalID = HospitalID;
 		this.Password = password;
-		this.User_Role = role;
 	}
 
 	public String getHospitalID(){
