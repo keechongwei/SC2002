d647f06b14cca3bda6b568b874d847f3b263cfab
keechongwei <keechongwei@gmail.com>
2024-11-17 14:40:43 +0800

    removed unused libraries from Login

--- Login.java
+++ Login.java
@@ -1,11 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Login {
     enum Role{Unknown,Patient,Doctor,Pharmacist,Administrator}
