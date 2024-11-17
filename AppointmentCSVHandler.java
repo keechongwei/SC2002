19f510d000b9b4c4788e39d03c6563b76dad6cfe
keechongwei <keechongwei@gmail.com>
2024-11-17 22:18:32 +0800

    ported csv functions in apptManager to apptCSVHandler

---
+++ AppointmentCSVHandler.java
@@ -0,0 +1,84 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class AppointmentCSVHandler {
+    public static String appointmentsCSVHeader = "Date;Time;Appointment ID; DoctorID; PatientID;Appointment Status; OutcomeDate | Outcome Time |  Type Of Service | Medication Name ^ Medication Status ^ Medication Dosage | Consultation Notes";
+    public static final File csvFile = new File("appointments.csv");
+
+        // write header to Appointments CSV file
+    public static void writeHeader(String header){
+        try (FileWriter writer = new FileWriter(csvFile, true)) {
+            writer.write(header + "\n");
+        } catch (IOException e) {
+            System.out.println("Error writing header to CSV file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadAppointmentsFromCSV(File csvFile) {
+    boolean headerline = true;
+    try (Scanner scanner = new Scanner(csvFile)) {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            // ignore headerline
+            if(headerline){
+                headerline = false;
+                continue;
+            }
+            String[] ApptSlotfields = line.split(";");
+            String date = ApptSlotfields[0];
+            String time = ApptSlotfields[1];
+            String appointmentID = ApptSlotfields[2];
+            String doctorID = ApptSlotfields[3];
+            String patientID = ApptSlotfields[4];
+            AppointmentStatus status = AppointmentStatus.valueOf(ApptSlotfields[5]);
+            String outcome = ApptSlotfields.length > 6 ? ApptSlotfields[6] : "";
+            AppointmentOutcomeRecord outcomeRecord = null;
+            if (!outcome.equals("null")){
+                outcomeRecord = AppointmentOutcomeRecord.fromCSV(outcome);
+            }
+            AppointmentManager.appointmentSlotArray.add(new AppointmentSlot(date, time,status,doctorID,patientID,outcomeRecord,appointmentID));
+            AppointmentManager.nextAppointmentID++;
+        }
+        System.out.println("Appointments loaded successfully!");
+    } catch (FileNotFoundException e) {
+        System.out.println("Unable to load appointments from file.");
+        e.printStackTrace();
+    }   
+    }
+    // function to add new values of AppointmentSlots
+    public static void appendAppointmentToCSV(AppointmentSlot slot) {
+        try (FileWriter writer = new FileWriter(csvFile, true)) {
+            writer.write(slot.toCSV() + "\n");
+        } catch (IOException e) {
+            System.out.println("Error writing appointment to CSV file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateAppointmentsCSV(){
+        AppointmentCSVHandler.writeHeader(appointmentsCSVHeader);
+        AppointmentCSVHandler.writeCSV(AppointmentManager.appointmentSlotArray);
+    }
+
+        // function to edit values of AppointmentSlots without creating rows
+    public static void writeCSV(List<AppointmentSlot> appointments) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
+            // Write the header line first
+            bw.write(appointmentsCSVHeader);
+            bw.newLine();
+            for (AppointmentSlot appointment : appointments) {
+                bw.write(appointment.toCSV());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing CSV file: " + e.getMessage());
+        }
+    }
+
+}
--- AppointmentManager.java
+++ AppointmentManager.java
@@ -1,36 +1,14 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class AppointmentManager {
     public static List<AppointmentSlot> appointmentSlotArray = new ArrayList<>();
-    public static String appointmentsCSVHeader = "Date;Time;Appointment ID; DoctorID; PatientID;Appointment Status; OutcomeDate | Outcome Time |  Type Of Service | Medication Name ^ Medication Status ^ Medication Dosage | Consultation Notes";
-    public static final File csvFile = new File("appointments.csv");
     // decides how many slots will there be in a day
     public static int numberofSlots = 10;
     public static int nextAppointmentID = 1;
 
-    public static void updateAppointmentsCSV(){
-        AppointmentManager.writeHeader(AppointmentManager.appointmentsCSVHeader);
-        AppointmentManager.writeCSV(AppointmentManager.appointmentSlotArray);
-    }
-    
-    // write header to Appointments CSV file
-    public static void writeHeader(String header){
-        try (FileWriter writer = new FileWriter(csvFile, true)) {
-            writer.write(header + "\n");
-        } catch (IOException e) {
-            System.out.println("Error writing header to CSV file.");
-            e.printStackTrace();
-        }
-    }
     public static void makeDailyAppointments(List<Doctor> doctors){
         // hour determines starting hour
         int hour = 9;
@@ -54,33 +32,8 @@ public class AppointmentManager {
     
     public static void addAppointment(AppointmentSlot slot) {
         appointmentSlotArray.add(slot);
-        appendAppointmentToCSV(slot);
+        AppointmentCSVHandler.appendAppointmentToCSV(slot);
         nextAppointmentID++;
-    }
-    
-    // function to edit values of AppointmentSlots without creating rows
-    public static void writeCSV(List<AppointmentSlot> appointments) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
-            // Write the header line first
-            bw.write(appointmentsCSVHeader);
-            bw.newLine();
-            for (AppointmentSlot appointment : appointments) {
-                bw.write(appointment.toCSV());
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error writing CSV file: " + e.getMessage());
-        }
-    }
-
-    // function to add new values of AppointmentSlots
-    public static void appendAppointmentToCSV(AppointmentSlot slot) {
-        try (FileWriter writer = new FileWriter(csvFile, true)) {
-            writer.write(slot.toCSV() + "\n");
-        } catch (IOException e) {
-            System.out.println("Error writing appointment to CSV file.");
-            e.printStackTrace();
-        }
     }
 
     // for patients to see pending appointments, get all slots depending on DoctorID
@@ -131,49 +84,17 @@ public class AppointmentManager {
         
     }
 
-    public static void loadAppointmentsFromCSV(File csvFile) {
-        boolean headerline = true;
-        try (Scanner scanner = new Scanner(csvFile)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                // ignore headerline
-                if(headerline){
-                    headerline = false;
-                    continue;
-                }
-                String[] ApptSlotfields = line.split(";");
-                String date = ApptSlotfields[0];
-                String time = ApptSlotfields[1];
-                String appointmentID = ApptSlotfields[2];
-                String doctorID = ApptSlotfields[3];
-                String patientID = ApptSlotfields[4];
-                AppointmentStatus status = AppointmentStatus.valueOf(ApptSlotfields[5]);
-                String outcome = ApptSlotfields.length > 6 ? ApptSlotfields[6] : "";
-                AppointmentOutcomeRecord outcomeRecord = null;
-                if (!outcome.equals("null")){
-                    outcomeRecord = AppointmentOutcomeRecord.fromCSV(outcome);
-                }
-                appointmentSlotArray.add(new AppointmentSlot(date, time,status,doctorID,patientID,outcomeRecord,appointmentID));
-                nextAppointmentID++;
-            }
-            System.out.println("Appointments loaded successfully!");
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to load appointments from file.");
-            e.printStackTrace();
-        }
-    }
-
     // initialises appointment array
     public static void initialiseAppointments() {
-        if (!((AppointmentManager.csvFile).exists()) || (AppointmentManager.csvFile).length() == 0) {
+        if (!((AppointmentCSVHandler.csvFile).exists()) || (AppointmentCSVHandler.csvFile).length() == 0) {
             // File doesn't exist or is empty, create daily appointments
             System.out.println("appointments.csv is empty or missing. Generating daily appointments...");
-            AppointmentManager.writeHeader(AppointmentManager.appointmentsCSVHeader);
+            AppointmentCSVHandler.writeHeader(AppointmentCSVHandler.appointmentsCSVHeader);
             AppointmentManager.makeDailyAppointments(StaffManager.doctors); // Replace getStaffList() with your method to get the staff data
         } else {
             // File exists and is not empty, load appointments from the CSV
             System.out.println("Loading appointments from appointments.csv...");
-            AppointmentManager.loadAppointmentsFromCSV(AppointmentManager.csvFile);
+            AppointmentCSVHandler.loadAppointmentsFromCSV(AppointmentCSVHandler.csvFile);
         }
     }
     // Other functions for filtering by patient, status, etc.
