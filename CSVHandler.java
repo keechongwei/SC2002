1c1b65448ae8514d1f0717267e839408475546e6
keechongwei <keechongwei@gmail.com>
2024-11-21 02:21:36 +0800

    Javadoc for CSVHandler

--- CSVHandler.java
+++ CSVHandler.java
@@ -1,3 +1,10 @@
+/**
+ * interface class
+ * for all CSVHandlers to implement
+ * @see AppointmentCSVHandler
+ * @see InventoryCSVHandler
+ * @see PatientManagerCSVHandler
+ */
 interface CSVHandler {
     public static void loadCSV(){};
     public static void writeCSV(){};
