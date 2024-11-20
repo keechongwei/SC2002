891b4b98f86e9a1bc79c39aac229d0f4d582b1f0
keechongwei <keechongwei@gmail.com>
2024-11-21 02:16:31 +0800

    Javadoc for AppointmentSlot

--- AppointmentSlot.java
+++ AppointmentSlot.java
@@ -22,7 +22,18 @@ public class AppointmentSlot {
         this.appointmentID = "APT" + String.valueOf(AppointmentManager.nextAppointmentID);
     }
 
-    // for constructing appointments from CSV
+    /**
+     * Constructor for AppointmentSlot
+     * @param date String representing date in format "YYYY-MM-DD"
+     * @param time String representing time in format "HH:mm"
+     * @param status AppointmentStatus representing one of the Appointment Statuses
+     * @param doctorID String representing doctorID
+     * @param patientID String representing patientID
+     * @param outcome AppointmentOutcomeRecord object
+     * @param appointmentID String representing appointmentID
+     * @see AppointmentOutcomeRecord
+     * @see AppointmentStatus
+     */
     public AppointmentSlot(String date, String time, AppointmentStatus status, String doctorID, String patientID, AppointmentOutcomeRecord outcome, String appointmentID) {
         this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.time = LocalTime.parse(time,  DateTimeFormatter.ofPattern("HH:mm"));
@@ -32,46 +43,100 @@ public class AppointmentSlot {
         this.appointmentID = appointmentID;
         this.outcome = outcome;
     }
+    
+    /**
+     * Set method for changing patientID
+     * @param patientID String representing new patientID
+     * @return void
+     */
     public void setPatientID(String patientID){
         this.patientID = patientID;
     }
 
+    /**
+     * Get method for retrieving patientID
+     * @return String containing information of patientID
+     */
     public String getPatientID() {
         return this.patientID;
     }
 
+    /**
+     * Get method for retrieving appointmentID
+     * @return String containing information of appointmentID
+     */
     public String getAppointmentID(){
         return this.appointmentID;
     }
-    
+
+    /**
+     * Set method for changing doctorID
+     * @param doctorID String representing new doctorID
+     * @return void
+     */
     public void setDoctorID(String doctorID){
         this.doctorID = doctorID;
     }
-
+    /**
+     * Get method for retrieving doctorID
+     * @return String containing information of doctorID
+     */
     public String getDoctorID() {
         return this.doctorID;
     }
 
+    /**
+     * Get method for retrieving date
+     * @return LocalDate containing information of date
+     */
     public LocalDate getDate() {
         return this.date;
     }
 
+    /**
+     * Get method for retrieving time
+     * @return LocalTime containing information of time
+     */
     public LocalTime getTime() {
         return this.time;
     }
 
+    /**
+     * Get method for retrieving status
+     * @return AppointmentStatus containing appointment status
+     * @see AppointmentStatus
+     */
     public AppointmentStatus getStatus() {
         return this.status;
     }
 
+    /**
+     * Set method for changing AppointmentStatus
+     * @param status AppointmentStatus representing new status
+     * @return void
+     * @see AppointmentStatus
+     */
     public void setStatus(AppointmentStatus status) {
         this.status = status;
     }
 
+    /**
+     * Get method for retrieving AppointmentOutcomeRecord
+     * @return AppointmentOutcomeRecord containing information in AppointmentOutcomeRecord
+     * @see AppointmentOutcomeRecord
+     */
     public AppointmentOutcomeRecord getAppointmentOutcomeRecord() {
         return this.outcome;
     }
 
+    /**
+     * Updates information in AppointmentOutcomeRecord attribute
+     * @param date LocalDate object containing information on date
+     * @param time LocalTime object containing information on time
+     * @param serviceType String object containing information on the TypeOfService
+     * @param pres Prescription object containing information on prescribed medication and dosage
+     * @param consult String object containing information on consultationNotes
+     */
     public void updateAppointmentOutcomeRecord(LocalDate date,LocalTime time, String serviceType, Prescription pres, String consult) {
         TypeOfService typeOfServiceEnum;
     try {
@@ -101,10 +166,12 @@ public class AppointmentSlot {
     }
     }
 
-    // public Patient getPatient() {
-    //     return Patient.getPatientByID(patientID);
-    // }
 
+    /**
+     * Returns AppointmentSlot as a String to be added to appointments.csv
+     * @param void
+     * @return String object containing of AppointmentSlot as a String delimited appropriately
+     */
     public String toCSV() {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
