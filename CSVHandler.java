f3720163b4aa851a37b26270facf4841ac9443a9
keechongwei <keechongwei@gmail.com>
2024-11-21 03:11:09 +0800

    edited Javadoc for Interface

--- CSVHandler.java
+++ CSVHandler.java
@@ -1,6 +1,8 @@
 /**
- * interface class
- * for all CSVHandlers to implement
+ * An Interface for all CSVHandlers to implement
+ * @author SCSKGroup2
+ * @version 1.0
+ * @since 2024-11-21
  * @see AppointmentCSVHandler
  * @see InventoryCSVHandler
  * @see PatientManagerCSVHandler
