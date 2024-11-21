0c240efae729cf3dfb1fd50925e5b6ae04c9fb9a
keechongwei <keechongwei@gmail.com>
2024-11-21 15:26:38 +0800

    Javadoc for prescription status

--- PrescriptionStatus.java
+++ PrescriptionStatus.java
@@ -1,3 +1,8 @@
+/**
+ * Enumerator containing possible status of Prescription
+ * Pending - Doctor prescribed medication, pending Pharmacist approval
+ * Dispensed - Pharmacist Approved Prescription Request
+ */
 public enum PrescriptionStatus {
     PENDING,
     DISPENSED
